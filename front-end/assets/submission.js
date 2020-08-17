eede96b96971d1e8c0f6fd64c81510803b168b21
farizbd <69798046+farizbd@users.noreply.github.com>
2020-08-17 18:46:49 +0700

    Add files via upload

---
+++ front-end/assets/submission.js
@@ -0,0 +1,9 @@
+const firstName = prompt("Siapa nama depanmu?");
+const lastName = prompt("Siapa nama belakangmu?");
+ 
+const user = {
+       first: firstName,
+       last: lastName,
+};
+
+alert("hai.. " + user.first + " " + user.last + " mari bersama kita cegah corona.");
